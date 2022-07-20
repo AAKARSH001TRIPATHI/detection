b59bbf283bf9c311880a53870fd8c1b857487d23
AAKARSH001TRIPATHI <109679773+AAKARSH001TRIPATHI@users.noreply.github.com>
2022-07-20 20:44:01 +0530

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -7,7 +7,7 @@ detectface=cv2.CascadeClassifier("face.xml")
 st.title("Face Detection System")
 if(choice=="HOME"):
     st.header("welcome")
-    st.image("icon1.gif")
+    st.image("https://tenor.com/view/face-recognition-gif-19358861")
 elif(choice=="IMAGE"):
     img=st.file_uploader("please upload your image")
     if img:
